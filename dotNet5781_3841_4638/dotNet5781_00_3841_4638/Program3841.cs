d8c8dab0b48be2889301423db84f8b694d574a70
Shira Shevkes <shevkes@g.jct.ac.il>
2020-10-22 21:04:18 +0300

    intial code + Welcome3841

---
+++ dotNet5781_3841_4638/dotNet5781_00_3841_4638/Program3841.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_3841_4638
+{
+    partial class Program
+    {
+        static partial void Welcome3841()
+        {
+            Console.WriteLine("I am also here");
+        }
+    }
+}
