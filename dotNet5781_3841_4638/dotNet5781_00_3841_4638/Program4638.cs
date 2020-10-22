c6ca148459602c78707f5d37b1cb91b62b2c4f97
Ayala Cohen <ayacohen@g.jct.ac.il>
2020-10-22 20:33:35 +0300

    intial code- Welcome4638

---
+++ dotNet5781_3841_4638/dotNet5781_00_3841_4638/Program4638.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_3841_4638
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome4638();
+            Welcome3841();
+            Console.ReadKey();
+        }
+        static partial void Welcome3841();
+        private static void Welcome4638()
+        {
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+
+    }
+}
