76b59d00eaa6cec15788b9d6b99bfa0cc189a27d
Ayala Cohen <ayacohen@g.jct.ac.il>
2020-11-03 14:00:47 +0200

    main and class bus

---
+++ dotNet5781_3841_4638/dotNet5781_01_3841_4638/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace dotNet5781_01_3841_4638
+{
+    enum choice { insert = 1, select, refuel, print};
+    public static class Program
+    {
+        public static List<Bus> list;
+        public static Random r;
+        public static void Main(string[] args)
+        {
+            list = new List<Bus>();
+            r = new Random(DateTime.Now.Millisecond);
+            while (true)
+            {
+                Console.WriteLine("Select a number from the following menu:");
+                Console.WriteLine("1 To add a bus");
+                Console.WriteLine("2 To select a bus from the bus list");
+                Console.WriteLine("3 To do maintenance or refueling for the bus");
+                Console.WriteLine("4 To view the mileage and license number of the company's vehicles");
+                Console.WriteLine("Any other number to exit");
+                choice c = (choice)Enum.Parse(typeof(choice), Console.ReadLine());
+
+                switch (c)
+                {
+                    case choice.insert:
+                        insertBus(list);
+                        break;
+                    case choice.select:
+                        selectBus(list);
+                        break;
+                    case choice.refuel:
+                        refuelOrTreat(list);
+                        break;
+                    case choice.print:
+                        printDetails(list);
+                        break;
+                    default:
+                        return;
+                        
+                }
+            }
+        }
+        public static bool checkNumAndDate(string num,DateTime date)//Checks if the date matches the license number
+        {
+            if(num.Length<7||num.Length>8)
+            {
+                Console.WriteLine("The license number you entered is incorrect");
+                return false;
+            }
+            if((date.Year<2018&&num.Length!=7)||(date.Year >= 2018 && num.Length != 8))
+            {
+                Console.WriteLine("The license number you entered does not match the dateThe number you entered is incorrect");
+                return false;
+            }
+            Console.WriteLine("The license number matches the date you entered");
+            return true;
+        }
+        public static void insertBus(List<Bus> buses)
+        {
+            double totalKm;
+            DateTime startDate = new DateTime();
+            Console.WriteLine("Enter your license number:");
+            string licenseNumber = Console.ReadLine();
+            bool checkDate = false;
+            while (checkDate == false)
+            {
+                Console.WriteLine("Enter your activity start date according to this format dd/MM/yyyy:");
+                string startDatestr = Console.ReadLine();
+                checkDate = DateTime.TryParseExact(startDatestr, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out startDate);
+            }
+            
+           if ( checkNumAndDate(licenseNumber, startDate))
+            {
+                if (Bus.check(licenseNumber))
+                {
+                    Console.WriteLine("Enter how many km you have traveled so far");
+                    string totalKmstr= Console.ReadLine();
+                    bool total = double.TryParse(totalKmstr, out totalKm);
+                    Bus bus = new Bus();
+                    bus.licenseNumber = licenseNumber;
+                    bus.StartDate = startDate;
+                    bus.TotalKm = totalKm;
+                    bus.fuel = 0;
+                    bus.lastKmTreat = 0;
+                    bus.lastTreatment = startDate;
+                    buses.Add(bus);
+                    Console.WriteLine("The data was successfully absorbed");
+                }
+            }
+        }
+        public static void selectBus(List<Bus>buses)
+        {
+            Console.WriteLine("Enter your license number:");
+            string licenseNumber = System.Console.ReadLine();
+            if (!Bus.check(licenseNumber))
+            {
+                Console.WriteLine("The license number you entered is incorrect");
+                return;
+            }
+            bool exist = false;
+            foreach (Bus bus in buses)
+            {
+                if (bus.licenseNumber == licenseNumber && exist == false)
+                {
+                    exist = true;
+                }
+                Console.Write("The number of kilometers in the requested trip are:");
+                double km = r.Next(0, 1201);
+                Console.Write(km);
+                if (bus.CanTravel(km))
+                {
+                    bus.TotalKm += km;
+                    bus.fuel -= km;
+                    Console.WriteLine("You can make the trip");
+                    return;
+                }
+                else
+                    return;
+            }
+                if (!exist)
+                {
+                    Console.WriteLine(" There is no bus with the license number you entered");
+                }
+        }
+        public static void refuelOrTreat(List<Bus> buses) //The user selects the desired treatment
+        {
+            Console.WriteLine("Enter your license number");
+            String licenseNumber = Console.ReadLine();
+            bool c = false;
+            foreach (Bus bus in buses)
+            {
+                if (bus.licenseNumber == licenseNumber && c == false)
+                {
+                    c = true;
+                    Console.WriteLine(" For a treatment enter 't',for refuel enter 'r',");
+                    Char ans = Console.ReadLine()[0];
+                    while (ans != 'r' && ans != 't')
+                    {
+                        Console.WriteLine(" Wrong answer");
+                        ans = Console.ReadLine()[0];
+                    }
+                    if (ans == 't')
+                    {
+                        bus.Treatment();
+                        Console.WriteLine("The treatment was performed successfully");
+                    }
+                    if (ans == 'r')
+                    {
+                        bus.Refuel();
+                        Console.WriteLine("The fuel tank was successfully filled");
+                    }
+                }
+            }
+            if (c == false)
+                Console.WriteLine(" There is no bus with the license number you entered");
+        }
+        public static void printDetails(List<Bus> buses)
+        {
+            foreach (Bus bus in buses)
+            {
+                Console.WriteLine(bus);
+            }
+        }
+        
+
+    }
+}
---
+++ dotNet5781_3841_4638/dotNet5781_01_3841_4638/bus.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_01_3841_4638
+{
+    public class Bus
+    {
+        public string licenseNumber { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime lastTreatment { get; set; } //The last date the bus received treatment
+        public double TotalKm { get; set; }
+        public double lastKmTreat { get; set; } //The amount of miles traveled since the last treatment
+        public double fuel { get; set; }
+        public static bool check(string c) //Check if the license number is correct
+        {
+            int check;
+            if (Int32.TryParse(c, out check))
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool CanTravel(double km)
+        {
+            if (((TotalKm - lastKmTreat) + km >= 20000) || ((DateTime.Now - lastTreatment).TotalDays >= 365))
+            {
+                Console.WriteLine(" The bus needs treatment");
+                return false;
+            }
+            if (fuel - km < 0)
+            {
+                Console.WriteLine(" The bus is missing fuel");
+                return false;
+            }
+            return true;
+        }
+        public void Refuel()
+        {
+            fuel = 1200;
+        }
+        public void Treatment()
+        {
+            lastTreatment = DateTime.Now;
+            lastKmTreat = TotalKm;
+        }
+        public override string ToString() //Displays the mileage and license number of the bus.
+        {
+            string prefix, middle, suffix;
+            if (StartDate.Year < 2018)
+            {
+                prefix = licenseNumber.Substring(0, 2);
+                middle = licenseNumber.Substring(2, 3);
+                suffix = licenseNumber.Substring(5, 2);
+            }
+            else
+            {
+                prefix = licenseNumber.Substring(0, 3);
+                middle = licenseNumber.Substring(3, 2);
+                suffix = licenseNumber.Substring(5, 3);
+            }
+            string finalLicNum = String.Format("{0}-{1}-{2}", prefix, middle, suffix);
+            return "License Number: " + finalLicNum + " Total KM: " + TotalKm;
+
+        }
+    }
+}
